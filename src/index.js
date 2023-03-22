99bc3023ffdf5cc82f7715dfc316b5105056e53b
Andrew Jreij <andrew.jreij@gmail.com>
2023-03-22 00:51:53 -0400

    redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
@@ -11,12 +11,12 @@ import filterReducer from './reducers/filterReducer'
 // import { createNote } from './reducers/noteReducer'
 // import { filterChange } from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 // console.log(store.getState())
 // store.subscribe(() => console.log(store.getState()))
